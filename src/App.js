308b255b40731d30cbb47610859da213318cb23f
dmitry <darron555@live.ru>
2018-12-28 09:56:39 +0300

    Start

--- src/App.js
+++ src/App.js
@@ -1,102 +1,31 @@
-import React, {Component} from 'react';
-import './App.scss';
-import Car from './Car/Car'
-import ErrorBoundary from './ErrorBoundary/ErrorBoundary'
-import Counter from './Counter/Counter'
-
-
-export const ClickedContext = React.createContext(false)
-
+import React, {Component} from 'react'
+import './App.scss'
+import About from './About/About'
+import Cars from './Cars/Cars'
 
 class App extends Component {
+  render() {
 
-    constructor(props) {
-        super(props)
+    return (
+      <div>
+        <nav className="nav">
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/about">About</a>
+            </li>
+          </ul>
+        </nav>
 
-        this.state = {
-            cars: [
-                {name: 'Ford', year: 1988},
-                {name: 'Mazda', year: 2000}
-            ],
-            pageTitle: 'React title',
-            showCars: true,
-            clicked: false
-        }
-    }
+        <hr/>
+        <About />
 
-    renderCars() {
-
-        if (!this.state.showCars) {
-            return null;
-        }
-
-        return this.state.cars.map((car, index) => {
-            return <Car
-                key={index}
-                index={index}
-                name={car.name}
-                year={car.year}
-                onChangeName={event => this.onChangeName(event.target.value, index)}
-                deleteCar={this.deleteCarHandle.bind(this, index)}
-            />
-        })
-    }
-
-    onChangeName = (name, index) => {
-        let cars = [...this.state.cars];
-        cars[index].name = name;
-
-        this.setState({cars})
-
-    }
-
-    deleteCarHandle (index) {
-        let cars = [...this.state.cars];
-        cars.splice(index, 1)
-
-        this.setState({cars})
-
-    }
-
-    toggleCars = () => {
-        this.setState({showCars: !this.state.showCars})
-
-    }
-
-
-    render() {
-        const divStyle = {
-            textAlign: 'center'
-        }
-
-        return (
-            <div style={divStyle}>
-                <h1>{this.state.pageTitle}</h1>
-                <h1>{this.props.title}</h1>
-                <button className='AppButton' onClick={this.toggleCars}>Toggle</button>
-                <button onClick={() => this.setState({clicked: true})}>Clicked 2</button>
-
-                <ClickedContext.Provider value={this.state.clicked}>
-                    <Counter  />
-                </ClickedContext.Provider>
-
-
-                <div style={
-                    {
-                        width: 400,
-                        margin: 'auto',
-                        paddingTop: 20
-                    }
-                }>
-                    <ErrorBoundary>
-                        {this.renderCars()}
-                    </ErrorBoundary>
-
-                </div>
-
-            </div>
-        )
-    }
+        <Cars />
+      </div>
+    );
+  }
 }
 
-export default App;
+export default App
